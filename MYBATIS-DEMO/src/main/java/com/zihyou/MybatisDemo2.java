7e31e67c5bbb57348498cf08197ce2f3bded2c56
zihuaiyou <zihuaiyou1996@qq.com>
2022-10-17 00:34:22 +0800

    44

---
+++ MYBATIS-DEMO/src/main/java/com/zihyou/MybatisDemo2.java
@@ -0,0 +1,34 @@
+package com.zihyou;
+
+import com.zihyou.mapper.StuMapper;
+import com.zihyou.pojo.StuInfo;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisDemo2 {
+    //Mapper代理
+    public static void main(String[] args) throws IOException {
+        //加载核心配置文件获取SqlSessionFactory对象
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //获取sqlSession对象，用于执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行sql
+//         List<StuInfo> stuInfos = sqlSession.selectList("stu.selectStuInfo");
+        StuMapper stuMapper = sqlSession.getMapper(StuMapper.class);
+        List<StuInfo> stuInfos = stuMapper.selectStuInfo();
+        System.out.println(stuInfos);
+
+        //释放资源
+        sqlSession.close();
+    }
+}
---
+++ MYBATIS-DEMO/src/main/java/com/zihyou/mapper/StuMapper.java
@@ -0,0 +1,9 @@
+package com.zihyou.mapper;
+
+import com.zihyou.pojo.StuInfo;
+
+import java.util.List;
+
+public interface StuMapper {
+    List<StuInfo> selectStuInfo();
+}
